7e539b4ba3aac079412b1b48a5cc8ccc2f061456
Ratnesh Maurya <85143283+ratnesh-maurya@users.noreply.github.com>
2024-10-21 19:17:25 +0530

    patch

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-// const { configureRuntimeEnv } = require("next-runtime-env/build/configure");
 
-// configureRuntimeEnv();
 
-module.exports = nextConfig;
+export default nextConfig;
