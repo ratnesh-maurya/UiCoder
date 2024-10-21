db4e47327fbbb19ea5e59b7c2281f41b6dcb2ef4
Ratnesh Maurya <85143283+ratnesh-maurya@users.noreply.github.com>
2024-10-21 18:57:58 +0530

    runtime environment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+const { configureRuntimeEnv } = require("next-runtime-env/build/configure");
+
+configureRuntimeEnv();
 
 export default nextConfig;
